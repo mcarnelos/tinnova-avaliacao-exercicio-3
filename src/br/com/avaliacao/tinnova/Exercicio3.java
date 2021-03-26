efe1e5dbf551802aea60ba31767376706c69b613
Murilo Carnelos <muriloc.c@hotmail.com>
2021-03-26 16:48:14 -0300

    Exercicio 3

---
+++ src/br/com/avaliacao/tinnova/Exercicio3.java
@@ -0,0 +1,22 @@
+package br.com.avaliacao.tinnova;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		
+		
+		int x = 1;
+		int num = 6;
+						
+		for (int i=1; i<=num; i++) {
+			
+			x *= i;			
+			System.out.println( i + "! = " + x);	
+		}		
+	}	
+		
+	}
+
+
